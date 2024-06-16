babedcbaf0a1eb62145d8a132231511433b16d7a
Nya Steve <nyasteve29@gmail.com>
2024-06-16 22:30:17 +0100

    initial commit

---
+++ src/dao/PharmacyUtils.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author STEVEY
+ */
+public class PharmacyUtils {
+//    public static String billPath = "D:\\Documents\\storeManagementProjectBills";
+      public static String billPath = "D:\\Documents\\Store\\Stevey";  
+}
